36eb6f27c7244d47ec9e9d56cb49ea77c3e13345
Fredrik Pedersen <fredrikhp@gmail.com>
2020-10-08 13:14:54 +0200

    Updates

---
+++ precache-manifest.4e023c9a9c7dcff25a84345807fcbe3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f20df50b18b0ada43dc52cd3b1ef32a",
+    "url": "/Orakel_Queue_Client/index.html"
+  },
+  {
+    "revision": "47c4f9969d0dd9822205",
+    "url": "/Orakel_Queue_Client/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "236824b4be324ac9af29",
+    "url": "/Orakel_Queue_Client/static/css/main.e74facf6.chunk.css"
+  },
+  {
+    "revision": "47c4f9969d0dd9822205",
+    "url": "/Orakel_Queue_Client/static/js/2.e2274e22.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Orakel_Queue_Client/static/js/2.e2274e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236824b4be324ac9af29",
+    "url": "/Orakel_Queue_Client/static/js/main.a8026483.chunk.js"
+  },
+  {
+    "revision": "f10ea6ed42d18defce2c",
+    "url": "/Orakel_Queue_Client/static/js/runtime-main.938f9b4a.js"
+  },
+  {
+    "revision": "2c306d99642b4679794b890d6b313c91",
+    "url": "/Orakel_Queue_Client/static/media/oslomethvit.2c306d99.png"
+  }
+]);
