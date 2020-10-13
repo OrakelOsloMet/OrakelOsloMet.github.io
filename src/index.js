3b1c353db76ddd5c58b0de21ca7fec84ec4900f7
FredrikPedersen <fredrikhp@gmail.com>
2020-10-13 14:43:30 +0200

    Enabled use of AuthReducer. Conditionally creating a store with a logger or not dependent on environment. Misc cleanup.

--- src/index.js
+++ src/index.js
@@ -2,21 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom";
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
+import {createStore, combineReducers, applyMiddleware, compose} from "redux";
+import {Provider} from "react-redux";
 import thunk from "redux-thunk";
 
 import App from './App';
+import authReducer from "./store/reducers/authReducer";
 import {objectConditionalByEnvironment} from "./utilities/objectUtilities";
 import './index.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-const rootReducer = combineReducers({
-
-});
-
-//Logger for debugging purposes, not enabled in production
-const logger = objectConditionalByEnvironment(store => {
+//Logger for debugging Redux, should not be enabled in production
+const logger = store => {
     return next => {
         return action => {
             console.log("[Middleware] Dispatching: ", action);
@@ -25,20 +22,24 @@ const logger = objectConditionalByEnvironment(store => {
             return result;
         };
     };
-}, null);
+};
+
+const rootReducer = combineReducers({
+    auth: authReducer
+});
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
+
+//Conditionally creates a store with or without a logger, dependent on the environment.
+const store = objectConditionalByEnvironment(createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk))), createStore(rootReducer, composeEnhancers(applyMiddleware(thunk))));
 
 const app = (
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
 );
 
-ReactDOM.render(<Provider store={store}>{app}</Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(app, document.getElementById("root"));
 serviceWorker.unregister();
