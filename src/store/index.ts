2eda13ecd07edf2d7768fd672179a35ad8b1e585
Fredrik Pedersen <fredrikhp@gmail.com>
2021-01-14 20:54:54 +0100

    Converted index to TypeScript. Removed store logger.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import {combineReducers, compose, Store} from "redux";
+import {combineReducers, compose} from "redux";
 import authReducer from "./reducers/authReducer";
 import queueReducer from "./reducers/queueReducer";
 
@@ -7,18 +7,6 @@ declare global {
         __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
     }
 }
-
-//TODO Add proper types here instead of any
-export const storeLogger = (store: Store) => {
-    return (next: any) => {
-        return (action: any) => {
-            //console.log("[Middleware] Dispatching: ", action);
-            const result = next(action);
-            //console.log("[Middleware] next state: ", store.getState());
-            return result;
-        };
-    };
-};
 
 export const rootReducer = combineReducers({
     auth: authReducer,
