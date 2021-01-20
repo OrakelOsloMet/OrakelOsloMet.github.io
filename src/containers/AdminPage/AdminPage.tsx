4e5ea8c4bc66e8947c519b55bb72173e43d3c7cb
Fredrik Pedersen <fredrikhp@gmail.com>
2021-01-20 17:04:43 +0100

    Created rough sketch of admin page

--- src/containers/AdminPage/AdminPage.tsx
+++ src/containers/AdminPage/AdminPage.tsx
@@ -1,7 +1,43 @@
 import React from "react";
 
 const AdminPage = () => {
-    return (<h1>HELLO THERE</h1>)
+    return (
+        <>
+            <h1>Administrer Eksisterende Emner</h1>
+            <form>
+                <label htmlFor={"fag"}>Velg Emne</label>
+                <select id={"fag"}>
+                    <option>Fag 1</option>
+                    <option>Fag 2</option>
+                    <option>Fag 3</option>
+                </select>
+                <input type={"text"} placeholder={"Nytt Navn"}/>
+                <select>
+                    <option>Vår</option>
+                    <option>Høst</option>
+                </select>
+                <button>Lagre</button>
+                <button>Slett Emne</button>
+            </form>
+
+            <h1>Nytt Emne</h1>
+            <form>
+                <input placeholder={"Emnenavn"}/>
+                <select>
+                    <option>Vår</option>
+                    <option>Høst</option>
+                </select>
+                <button>Lagre Emne</button>
+            </form>
+
+            <h1>Kødata</h1>
+            <label htmlFor={"fag"}>Fra</label>
+            <input id={"exportFrom"} type={"date"}></input>
+            <label htmlFor={"exportTo"}>Til</label>
+            <input id={"exportTo"} type={"date"}></input>
+            <button>Eksporter Kødata</button>
+        </>
+    )
 }
 
 export default AdminPage;
