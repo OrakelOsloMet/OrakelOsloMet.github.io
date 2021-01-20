0bc13945e66a21660bcb365edf6082ce1908ac7b
Fredrik Pedersen <fredrikhp@gmail.com>
2021-01-20 17:11:31 +0100

    Made the sketch a little bit prettier on the eyes

--- src/containers/AdminPage/AdminPage.tsx
+++ src/containers/AdminPage/AdminPage.tsx
@@ -1,41 +1,41 @@
 import React from "react";
+import {ConfirmButton, DeleteButton} from "../../components/UI/Buttons/buttons";
 
 const AdminPage = () => {
     return (
         <>
-            <h1>Administrer Eksisterende Emner</h1>
-            <form>
-                <label htmlFor={"fag"}>Velg Emne</label>
-                <select id={"fag"}>
+            <h1 className={""}>Administrer Eksisterende Emner</h1>
+            <form className={"form-inline mt-5 mb-5"} style={{width: "50%", margin: "auto"}}>
+                <select className={"form-control ml-2 mr-2 mt-2"} id={"fag"}>
                     <option>Fag 1</option>
                     <option>Fag 2</option>
                     <option>Fag 3</option>
                 </select>
-                <input type={"text"} placeholder={"Nytt Navn"}/>
-                <select>
+                <input className={"form-control ml-2 mr-2 mt-2"} type={"text"} placeholder={"Nytt Navn"}/>
+                <select className={"form-control ml-2 mr-2 mt-2"}>
                     <option>Vår</option>
                     <option>Høst</option>
                 </select>
-                <button>Lagre</button>
-                <button>Slett Emne</button>
+                <ConfirmButton>Lagre</ConfirmButton>
+                <DeleteButton>Slett Emne</DeleteButton>
             </form>
 
             <h1>Nytt Emne</h1>
-            <form>
-                <input placeholder={"Emnenavn"}/>
-                <select>
+            <form className={"form-inline mt-5 mb-5"} style={{width: "50%", margin: "auto"}}>
+                <input className={"form-control ml-2 mr-2 mt-2"} placeholder={"Emnenavn"}/>
+                <select className={"form-control ml-2 mr-2 mt-2"}>
                     <option>Vår</option>
                     <option>Høst</option>
                 </select>
-                <button>Lagre Emne</button>
+                <ConfirmButton>Lagre Emne</ConfirmButton>
             </form>
 
             <h1>Kødata</h1>
-            <label htmlFor={"fag"}>Fra</label>
-            <input id={"exportFrom"} type={"date"}></input>
-            <label htmlFor={"exportTo"}>Til</label>
-            <input id={"exportTo"} type={"date"}></input>
-            <button>Eksporter Kødata</button>
+            <form className={"form-inline mt-5 mb-5"} style={{width: "50%", margin: "auto"}}>
+                <input id={"exportFrom"} type={"date"} className={"form-control ml-2 mr-2 mt-2"}></input>
+                <input id={"exportTo"} type={"date"} className={"form-control ml-2 mr-2 mt-2"}></input>
+                <ConfirmButton>Eksporter Kødata</ConfirmButton>
+            </form>
         </>
     )
 }
