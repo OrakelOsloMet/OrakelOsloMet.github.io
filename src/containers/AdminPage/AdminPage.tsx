5a79901978d8bf38237ec2150c704df18ea9c2d4
Fredrik Pedersen <fredrikhp@gmail.com>
2021-01-21 15:35:24 +0100

    Design sketch for admin page

--- src/containers/AdminPage/AdminPage.tsx
+++ src/containers/AdminPage/AdminPage.tsx
@@ -1,40 +1,79 @@
 import React from "react";
-import {ConfirmButton, DeleteButton} from "../../components/UI/Buttons/buttons";
+import {DeleteButton, SubmitButton} from "../../components/UI/Buttons/buttons";
 
 const AdminPage = () => {
     return (
         <>
-            <h1 className={""}>Administrer Eksisterende Emner</h1>
-            <form className={"form-inline mt-5 mb-5"} style={{width: "50%", margin: "auto"}}>
-                <select className={"form-control ml-2 mr-2 mt-2"} id={"fag"}>
-                    <option>Fag 1</option>
-                    <option>Fag 2</option>
-                    <option>Fag 3</option>
-                </select>
-                <input className={"form-control ml-2 mr-2 mt-2"} type={"text"} placeholder={"Nytt Navn"}/>
-                <select className={"form-control ml-2 mr-2 mt-2"}>
-                    <option>Vår</option>
-                    <option>Høst</option>
-                </select>
-                <ConfirmButton>Lagre</ConfirmButton>
-                <DeleteButton>Slett Emne</DeleteButton>
+            <h1 className={"mt-5"}>Administrer Emner</h1>
+            <form className={"form-row ml-5 mt-5"} style={{backgroundColor: "white", width: "50%"}}>
+                <div className={"col-md-2 form-group ml-5"}>
+                    <div className={"d-flex flex-column"}>
+                        <label htmlFor={"subject"}>Emne</label>
+                        <select className={"form-control"} id={"subject"}>
+                            <option>Fag 1</option>
+                            <option>Fag 2</option>
+                            <option>Fag 3</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className={"col-md-2 form-group"}>
+                    <div className={"d-flex flex-column"}>
+                        <label htmlFor={"subjectName"}>Emnenavn</label>
+                        <input className={"form-control"} type={"text"} placeholder={"Nytt Navn"}
+                               id={"subjectName"}/>
+                    </div>
+                </div>
+
+                <div className={"col-md-2 form-group"}>
+                    <div className={"d-flex flex-column"}>
+                        <label htmlFor={"semester"}>Semester</label>
+                        <select className={"form-control"} id={"semester"}>
+                            <option>Vår</option>
+                            <option>Høst</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className={"col-md-2 form-group"}>
+                    <div className={"d-flex flex-column"}>
+                        <label htmlFor="semester">&nbsp;</label>
+                        <SubmitButton>Lagre</SubmitButton>
+                    </div>
+                </div>
+
+                <div className={"col-md-2 form-group"}>
+                    <div className={"d-flex flex-column"}>
+                        <label htmlFor="semester">&nbsp;</label>
+                        <DeleteButton>Slett Emne</DeleteButton>
+                    </div>
+                </div>
             </form>
 
-            <h1>Nytt Emne</h1>
-            <form className={"form-inline mt-5 mb-5"} style={{width: "50%", margin: "auto"}}>
-                <input className={"form-control ml-2 mr-2 mt-2"} placeholder={"Emnenavn"}/>
-                <select className={"form-control ml-2 mr-2 mt-2"}>
-                    <option>Vår</option>
-                    <option>Høst</option>
-                </select>
-                <ConfirmButton>Lagre Emne</ConfirmButton>
-            </form>
+            <h1 className={"mt-5 mb-5"}>Kødata</h1>
 
-            <h1>Kødata</h1>
-            <form className={"form-inline mt-5 mb-5"} style={{width: "50%", margin: "auto"}}>
-                <input id={"exportFrom"} type={"date"} className={"form-control ml-2 mr-2 mt-2"}></input>
-                <input id={"exportTo"} type={"date"} className={"form-control ml-2 mr-2 mt-2"}></input>
-                <ConfirmButton>Eksporter Kødata</ConfirmButton>
+            <form className={"form-row ml-5 mt-5"}>
+
+                <div className={"col-md-2 form-group"}>
+                    <div className={"d-flex flex-column"}>
+                        <label htmlFor={"exportFrom"}>Fra</label>
+                        <input id={"exportFrom"} type={"date"} className={"form-control"}></input>
+                    </div>
+                </div>
+
+                <div className={"col-md-2 form-group"}>
+                    <div className={"d-flex flex-column"}>
+                        <label htmlFor={"exportTo"}>Til</label>
+                        <input id={"exportTo"} type={"date"} className={"form-control"}></input>
+                    </div>
+                </div>
+
+                <div className={"col-md-1 form-group"}>
+                    <div className={"d-flex flex-column"}>
+                        <label htmlFor="exportTo">&nbsp;</label>
+                        <SubmitButton>Eksporter</SubmitButton>
+                    </div>
+                </div>
             </form>
         </>
     )
