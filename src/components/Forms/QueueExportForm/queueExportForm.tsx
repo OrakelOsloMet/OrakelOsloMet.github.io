a3634d4bdf3d51f5f6b82daa716df03256925bb4
Fredrik Pedersen <fredrikhp@gmail.com>
2021-02-01 09:50:38 +0100

    Added authorization header to exportQueueData call

--- src/components/Forms/QueueExportForm/queueExportForm.tsx
+++ src/components/Forms/QueueExportForm/queueExportForm.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import {FILE_DOWNLOAD_CONFIG, FILE_INSTANCE as axios} from "../../../axiosAPI";
 import {SubmitButton} from "../../UI/Buttons/buttons";
 import {QUEUE_EXPORT_PATH} from "../../../constants/constants";
+import authHeader from "../../../httpHeaders/authHeader";
 
 const QueueExportForm = () => {
 
     const getQueueExport = () => {
-        axios.get(QUEUE_EXPORT_PATH)
+        axios.get(QUEUE_EXPORT_PATH, {headers: authHeader()})
             .then(response => {
-                FILE_DOWNLOAD_CONFIG("csv", "queueData", response.data);
+                FILE_DOWNLOAD_CONFIG("queueData", "csv", response.data);
             })
     }
 
