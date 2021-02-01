97d5290968a8916fabbe8126800419ec828a9ec3
Fredrik Pedersen <fredrikhp@gmail.com>
2021-02-01 11:38:51 +0100

    Added hand-waving icon to navbar

--- src/components/Navigation/Footer/footer.tsx
+++ src/components/Navigation/Footer/footer.tsx
@@ -3,6 +3,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Copyright from "../../Elements/Copyright/Copyright";
+import PanToolRoundedIcon from '@material-ui/icons/PanToolRounded';
 import useStyles from "./useStyles";
 
 const Footer = () => {
@@ -13,7 +14,7 @@ const Footer = () => {
             <CssBaseline />
             <footer className={classes.footer}>
                 <Container maxWidth="sm">
-                    <Typography variant="body1">This isn't the footer you are looking for.</Typography>
+                    <Typography variant="body1">This is not the footer you are looking for. <PanToolRoundedIcon/></Typography>
                     <Copyright />
                 </Container>
             </footer>
